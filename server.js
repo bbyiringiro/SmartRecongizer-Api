ea533fe31bbd554c789954fe522a292c197fc09a
Billy Byiringiro <billy@jeans-air.home>
2018-07-29 20:10:42 +0100

    deploying it

--- server.js
+++ server.js
@@ -13,18 +13,20 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
+    user : 'billy',
     password : '',
-    database : 'smart-brain'
+    database : 'SmartRecognition'
   }
 });
+
+
 
 const app = express();
 
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(database.users) })
+app.get('/', (req, res)=> { res.send('Access') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
